af479e3dd944bc22dd7520612cd493fc58102446
ajaybarath <ajaybarath3@gmail.com>
2021-10-05 18:21:57 +0530

    Refractored: to flip coin till head or tail wins 11 times

--- IfAndElseProblems/CoinFlip.js
+++ IfAndElseProblems/CoinFlip.js
@@ -1,8 +1,23 @@
-let coin= Math.floor(Math.random() * 10) % 2;
+let head  = 0;
+let tail = 0;
 
-if (coin == 0) {
-    console.log("It is a tail")
+while (head != 11 && tail != 11) {
+    let coin= Math.floor(Math.random() * 10) % 2;
+
+    if (coin == 0) {
+        console.log("It is a tail")
+        tail++;
+    }
+    else {
+        console.log("It is a head")
+        head++;
+    }
+}
+
+if (head == 11) {
+    console.log("Head won")
 }
 else {
-    console.log("It is a head")
+    console.log("Tail won")
 }
+

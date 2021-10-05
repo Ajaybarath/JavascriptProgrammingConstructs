ca3ab9635deddf91e56a4cbf08597a9c21d2b246
ajaybarath <ajaybarath3@gmail.com>
2021-10-05 17:57:16 +0530

    Refractored: to print prime numbers in a range

--- LoopProblems/PrimeNumber.js
+++ LoopProblems/PrimeNumber.js
@@ -3,21 +3,24 @@ const readline = require('readline').createInterface({
     output: process.stdout
 })
 
-readline.question(`Enter the number `, number => {
+readline.question(`Enter the number `, start => {
+    readline.question(`Enter the number `, end => {
 
-    let flag = 0;
+        for (var j = start; j < end; j++) {
+            let flag = 0;
 
-    for (var i=2; i <= number/2; i++) {
-        if(number % i == 0) {
-            flag = 1;
-            break;
+            for (var i = 2; i <= j / 2; i++) {
+                if (j % i == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (flag == 0)
+                console.log("Prime " + j)
+
         }
-    }
 
-    if (flag == 0) 
-        console.log("Prime")
-    else 
-        console.log("Not a Prime number")
-
-    readline.close();
+        readline.close();
+    })
 })

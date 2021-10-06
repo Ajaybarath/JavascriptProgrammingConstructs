ec47f396781a9fd655aa3ccff06d0d389159a549
ajaybarath <ajaybarath3@gmail.com>
2021-10-06 17:43:46 +0530

    Added: program for unit convertion

---
+++ MapProblems/SequenceProblem/UnitConvertion.js
@@ -0,0 +1,7 @@
+let ftToInch = 12;
+
+console.log("42 feet to inch : " + (42 * ftToInch));
+
+console.log("60 x 40 feet in meters : " + (60 * 40 * 0.3048))
+
+console.log("25 plots of 60 x 40 feet in meters : " + (25 * 60 * 40 * 0.3048))
